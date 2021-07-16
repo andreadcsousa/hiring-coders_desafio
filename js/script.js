94a4573a423d660dc1dcce9f84e1ea94d27456ec
Andrea Sousa <77935458+andreadcsousa@users.noreply.github.com>
2021-07-16 19:09:58 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+<button id="botaoEnviar" class="botao2" onclick="salvaContatoLocalStorage()">
+  Enviar
+</button>;
+
+function salvaContatoLocalStorage() {
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const telefone = document.getElementById("telefone").value;
+  const contato = { nome: nome, email: email, telefone: telefone };
+
+  return localStorage.setItem("contato", JSON.stringify(contato));
+}
